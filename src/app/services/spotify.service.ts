c2a7dcf694fc58962f5c61a4490aa486afc5bf77
Alberto <info@peralstudio.com>
2022-05-10 20:50:24 +0200

    ✔️ Update

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -17,7 +17,7 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQD6Syh8nNr27P29fmjRgRrL09Shd4qmSm3fpcRa3pyTn7muYNMInTTtzmo-bkZ-95mRqWGK7Eh1Ug0HHxY1Grm8WTGZYNU1HlMPFQW2OudhavaPCx297ABHZsPP5yQYzIN2YDQu0Ig_HS3N10XBFuIZm8TE04VU3iPoa6sGLkkKxNcNZw3NrmifnNmA5_cnBpw9jfSUHhzTseMWhvWcm8Bt7tNKVQ2_02Q5n9khJaTFGUtE42VMg3M8ZFELSlqM4_7ytl66au057OmM1-_lNg'
+      'Authorization': 'xxxxxxxxxx'
     });
 
     return this.http.get(url, { headers });
